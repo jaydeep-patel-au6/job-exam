2ed6d24ffc36be17694d1f52275d5921bef78e7e
Jaydeep Patel <58411819+jaydeep-patel-au6@users.noreply.github.com>
2020-12-28 16:37:16 +0530

    boiler plate

---
+++ moneyTap/js/index1.js
@@ -0,0 +1,19 @@
+// Implement pow(x, n), which calculates x raised to the power n (i.e. x^n).
+
+// Input: x = 2.00000, n = 10
+// Output: 1024.00000
+
+function pow(x, n){
+
+    var y = 1
+
+    for(i=1; i<=n || i>=n; i++){
+        y = y*x 
+    }
+    
+
+    console.log(y)
+
+}
+
+pow(2.0000, -2)
